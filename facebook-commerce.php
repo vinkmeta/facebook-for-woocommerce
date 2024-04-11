e02207d09b1b857fa0a6354dcf1020c68bc4ad49
Justin Palmer <228780+layoutd@users.noreply.github.com>
2024-04-11 15:43:12 +0200

    Remove messenger_chat from magic method

--- facebook-commerce.php
+++ facebook-commerce.php
@@ -389,7 +389,7 @@ class WC_Facebookcommerce_Integration extends WC_Integration {
 	 */
 	public function __get( $key ) {
 		// Add warning for private properties.
-		if ( in_array( $key, array( 'events_tracker', 'messenger_chat', 'background_processor' ), true ) ) {
+		if ( in_array( $key, array( 'events_tracker', 'background_processor' ), true ) ) {
 			/* translators: %s property name. */
 			_doing_it_wrong( __FUNCTION__, sprintf( esc_html__( 'The %s property is private and should not be accessed outside its class.', 'facebook-for-woocommerce' ), esc_html( $key ) ), '3.0.32' );
 			return $this->$key;
